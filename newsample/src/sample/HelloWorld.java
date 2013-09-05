ca919b8170ef2abe9ccb35363dd1cf7382fe7b1f
K Arvind Reddy <K Arvind Reddy@KArvindReddy>
2013-09-05 13:04:39 +0530

    new_Second Change

--- newsample/src/sample/HelloWorld.java
+++ newsample/src/sample/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("First commit");
+        System.out.println("second commit");
 	}
 
 }
