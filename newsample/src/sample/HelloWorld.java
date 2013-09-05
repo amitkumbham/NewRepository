6e0bfe1b7f30055f7c0237990a4990c08337a217
srikanth <srikanth@srikanth-PC>
2013-09-05 15:12:09 +0530

    error cleared

--- newsample/src/sample/HelloWorld.java
+++ newsample/src/sample/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
         System.out.println("second commit");
         System.out.println("cdfd");
        HelloWorld hw= new  HelloWorld();
-       me bun 
+        
         
 	}
 
